50c0c9f88688654986d6e915067089c819ad7a1f
Ian Vaillant <71671397+evaillock@users.noreply.github.com>
2023-05-15 13:06:57 -0400

    Updated notebooks and docstrings

--- checkX++Y.py
+++ checkX++Y.py
@@ -15,15 +15,15 @@ import glob
 #path that contains the IR files
 path = os.path.join(os.getcwd(), 'scrapedFTIR', 'ir','goodUnits')
     
-extension = 'jdx'
-all_files = glob.glob(os.path.join(path, "*.jdx"))
+extension = 'jdx' #extension for jcamp files
+all_files = glob.glob(os.path.join(path, "*.jdx")) #container for all of the files
 
-dtype = '(x++(y..y))'
-numNot = 0
+dtype = '(x++(y..y))' #need our jcamps to be in this format for further analysis
+numNot = 0 #number not in proper format counter
 for file in all_files:
     data = jcamp.jcamp_readfile(file)
     xydata = data.get('xydata', r'N\A').lower() != dtype
     if xydata == True:
         numNot += 1
 
-print(numNot)
+print(numNot) #gives a count of how many files were not usable
